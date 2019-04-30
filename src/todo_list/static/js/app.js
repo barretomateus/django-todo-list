60ad7fbcc5f04aba824cf03f8a04da628a97f2bf
Mateus Barreto <mateusbarreto@dcc.ufba.br>
2019-04-29 21:10:35 -0300

    Adiciona elementos na lista ao inserir no banco

--- src/todo_list/static/js/app.js
+++ src/todo_list/static/js/app.js
@@ -2,14 +2,22 @@ $(document).ready(function () {
     $('#add-button').click(function () {
         var atividade = $('#add-input').val(),
             csrfCookie = Cookies.get('csrftoken');
-        console.log(atividade);
+
         $.ajax({
             url: 'add_atividade',
             type: 'POST',
             headers: { 'X-CSRFToken': csrfCookie },
             data: { atividade: atividade },
             success: function (data) {
-                console.log(data);
+                var $tr = $('<tr>'), $th = $('<th>'), $td = $('<td>');
+
+                $th.prop('scope', 'row');
+                $th.html(data.id);
+                $td.html(data.descricao);
+
+                $tr.append($th, $td);
+
+                $('tbody').append($tr);
             },
         });
     });
